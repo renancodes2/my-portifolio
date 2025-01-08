1eac3505b8d715e78a4bc22af614be571e1936e3
renelps <renanlps616@gmail.com>
2025-01-08 09:33:04 -0300

    feat: initial commit - add full project

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'custom-white': '1px 1px 7px white',
+      },
+      colors: {
+        black: "#000000",
+        purpleDark: "#570057",
+        purpleLight: "#7e0c7d",
+        white: "#ffffff",
+      },
+      screens: {
+        'xs': '480px', // Breakpoint personalizado
+        '3xl': '1600px', // Outro exemplo
+      },
+    },
+  },
+  plugins: [],
+};
