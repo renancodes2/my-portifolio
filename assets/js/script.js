cbb7bcf64d05ecbaa710eafd9797fc830c271f95
renelps <renanlps616@gmail.com>
2025-01-10 11:21:51 -0300

    Adding a more responsive menu, including additional media queries, and adding more spacing.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,29 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuButton = document.getElementById("menu-button");
   const menu = document.getElementById("menu");
 
-  // Alternar o menu ao clicar no botão
-  menuButton.addEventListener("click", () => {
+  // Função para alternar a visibilidade do menu
+  const toggleMenu = () => {
     menu.classList.toggle("hidden");
+  };
+
+  // Abrir/fechar o menu ao clicar no botão
+  menuButton.addEventListener("click", toggleMenu);
+
+  // Fechar o menu ao clicar em qualquer lugar fora dele ou em um link
+  document.addEventListener("click", (event) => {
+    const isClickInsideMenu = menu.contains(event.target);
+    const isClickOnMenuButton = menuButton.contains(event.target);
+
+    if (!isClickInsideMenu && !isClickOnMenuButton) {
+      menu.classList.add("hidden");
+    }
   });
 
-  // Ocultar o menu ao clicar em um link
-  const menuLinks = menu.querySelectorAll("a");
-  menuLinks.forEach(link => {
+  // Fechar o menu ao clicar em um link dentro dele
+  const menuLinks = menu.querySelectorAll("li");
+  menuLinks.forEach((link) => {
     link.addEventListener("click", () => {
-      if (!menu.classList.contains("lg:flex")) {
-        menu.classList.add("hidden");
-      }
+      menu.classList.add("hidden");
     });
   });
 });
