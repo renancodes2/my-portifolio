b6050c155192d0e7866d5b9a45845b75552d305f
renelps <renanlps616@gmail.com>
2025-01-10 11:03:30 -0300

    config font-size and text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
-const menuButton = document.getElementById("menu-button");
-const menu = document.getElementById("menu");
+document.addEventListener("DOMContentLoaded", () => {
+  const menuButton = document.getElementById("menu-button");
+  const menu = document.getElementById("menu");
 
-menuButton.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
+  // Alternar o menu ao clicar no botão
+  menuButton.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+  });
+
+  // Ocultar o menu ao clicar em um link
+  const menuLinks = menu.querySelectorAll("a");
+  menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      if (!menu.classList.contains("lg:flex")) {
+        menu.classList.add("hidden");
+      }
+    });
+  });
 });
